3a4c5b3425fe921ec20b351670c4a246e29499e7
mdsab <mdsab@LAPTOP-9IUMOQI7>
2021-07-14 15:57:46 -0400

    Jenkins Inegration process test

--- src/sabeeltest/Test5.java
+++ src/sabeeltest/Test5.java
@@ -4,6 +4,10 @@ public class Test5 {
 
 	public static void main(String[] args) {
 		System.out.println("Testing Git Repo");
+		
+		System.out.println("Hurray i installed Jenkins");
+		
+		System.out.println("Added repo in Jenkins");
 
 	}
 
